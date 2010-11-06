08fd3fcdf880ea87f6ed66de19844ae80904adae
Brad Fritz <brad@fewerhassles.com>
2010-11-06 17:15:28 -0400

    improvements by Drew

--- donation-slider-form.php
+++ donation-slider-form.php
@@ -11,16 +11,12 @@
   <input type="hidden" name="bn" value="PP-DonationsBF:Donate_button_R1.png:NonHostedGuest"/>
 
   <div id="donation_box">
-    Donate
-    <input type="text" id="amount" style="border: 1px solid black; color: #f6931f; font-weight: bold;" />
-    to build solar powered wells.
+    Give <input type="text" id="amount" />
   </div>
 
   <div id="slider" style="float: left;"></div>
 
-  <input type="image" src="/wp-content/uploads/2010/07/Donate_button_R1.png" border="0" name="submit" alt=Donate to NuAfrica with PayPal"">
-    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
-  </input>
+
   <div id="donation_amounts">
     <a class="amount" style="margin-left: 10px">$10</a>
     <a class="amount" style="margin-left: 10px">$25</a>
@@ -32,9 +28,13 @@
     <a class="amount" style="margin-left: 10px">$20,000</a>
   </div>
   <div id="impact">
-    Your donation of <span id="donation_amount"></span>
-    will help <span id="people_text"></span>
-    have clean drinking water for 25 years.
+    <h3>Your donation of <span id="donation_amount"></span>
+    will:</h3>
+    <h2>Give <span id="people_text"></span> clean drinking water for 25 years</h2>
   </div>
+
+   <input type="image" src="/wp-content/uploads/2010/07/Donate_button_R1.png" border="0" name="submit" alt=Donate to NuAfrica with PayPal"">
+    <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1">
+  </input>
 </form>
 <?php ?>
